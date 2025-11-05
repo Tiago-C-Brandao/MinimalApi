81cd6c010979ef212baf2ccb0d19875dcd6a061d
Tiago-C-Brandao <tiago_curvelo@hotmail.com>
2025-11-05 05:21:06 -0300

    feat: add helpers and mocks

---
+++ Test/Mocks/AdminServiceMock.cs
@@ -0,0 +1,49 @@
+﻿using MinimalApi.Domain.DTOs;
+using MinimalApi.Domain.Entities;
+using MinimalApi.Domain.Interfaces;
+
+namespace Test.Mocks
+{
+    internal class AdminServiceMock : IAdminService
+    {
+        private static List<Admin> admins = new List<Admin>()
+        {
+            new Admin
+            {
+                Id = 1,
+                Email = "admin@test.com",
+                Password = "123456",
+                Role = "Adm"
+            },
+            new Admin
+            {
+                Id = 2,
+                Email = "editor@test.com",
+                Password = "123456",
+                Role = "Editor"
+            }
+        };
+
+        public Admin AddAdmin(Admin admin)
+        {
+            admin.Id = admins.Count() + 1;
+            admins.Add(admin);
+            return admin;
+        }
+
+        public List<Admin> All(int? page)
+        {
+            return admins;
+        }
+
+        public Admin? GetAdmin(int id)
+        {
+           return admins.Find(x => x.Id == id);
+        }
+
+        public Admin? Login(LoginDTO loginDTO)
+        {
+            return admins.Find(x => x.Email == loginDTO.Email && x.Password == loginDTO.Password);
+        }
+    }
+}
---
+++ Test/Mocks/VehicleServiceMock.cs
@@ -0,0 +1,73 @@
+﻿using MinimalApi.Domain.Entities;
+using MinimalApi.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Mocks
+{
+    internal class VehicleServiceMock : IVehicleService
+    {
+        private static List<Vehicle> vehicles = new List<Vehicle>()
+        {
+            new Vehicle
+            {
+                Id = 1,
+                Name = "NameTest1",
+                Brand = "BrandTest1",
+                Year = 2021
+            },
+            new Vehicle
+            {
+                Id = 2,
+                Name = "NameTest2",
+                Brand = "BrandTest2",
+                Year = 2022
+            },
+        };
+        public void AddVehicle(Vehicle vehicle)
+        {
+            vehicle.Id = vehicles.Count() + 1;
+            vehicles.Add(vehicle);
+        }
+
+        public List<Vehicle> All(int? page = 1, string? name = null, string? brand = null)
+        {
+            return vehicles;
+        }
+
+        public void DeleteVehicle(Vehicle vehicle)
+        {
+            var vehicleFind = vehicles.Find(x => x.Id == vehicle.Id);
+            if (vehicleFind != null)
+            {
+                vehicles.Remove(vehicleFind);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+
+        public Vehicle? GetVehicle(int id)
+        {
+            return vehicles.Find(x => x.Id == id);
+        }
+
+        public void UpdateVehicle(Vehicle vehicle)
+        {
+            var vehicleFind = vehicles.Find(x => x.Id == vehicle.Id);
+            if (vehicleFind != null)
+            {
+                vehicles.Remove(vehicleFind);
+                vehicles.Add(vehicle);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+    }
+}
